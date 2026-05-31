e4810fd7d47a1e5134225d67ce6e76a537fa140a
Vivekachooz <vivekachooz@gmail.com>
2026-05-31 18:49:01 +0530

    larry: Avoid shipping firmware by default
    * Due to the introduction of arb by Oneplus in OOS15, let users
      decide which fw they should be on. Any fw >= OOS14 is enough.
    
    Change-Id: I757bc4e796d33a4472aec01fa9b3f3f0db5aa518

--- Android.mk
+++ Android.mk
@@ -3,28 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),larry)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,0531847e4d691bf3a60a2f34554b1284c0f877fe)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,2eb215b7cae92d683443f55378685121d51011e5)
-$(call add-radio-file-sha1-checked,radio/core_nhlos.img,ae220403b28b1d814593658efd2e0c60dcd4e47e)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,58140ae5d5637fc10c70ab45757ff9f9ab35a10e)
-$(call add-radio-file-sha1-checked,radio/dsp.img,9606e98a9e1a1b886170e4858e7dc5ab2910548a)
-$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,13f5f33df779408b04cdfb3d78dd1ebbd3afb7b4)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,1c91284eee5b3e19eab404c0c9285c5f2f290f3d)
-$(call add-radio-file-sha1-checked,radio/hyp.img,a254d3050f3278755bad25dd120bb200cc8a9900)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,5843c51210de165d609c0ad00e48eac9efa4b2b9)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,1f0fd33ee73b31a73ef111dae4a5431e36422b92)
-$(call add-radio-file-sha1-checked,radio/modem.img,ace7c0da5ce7464e943c6ec8227472b332fb06c0)
-$(call add-radio-file-sha1-checked,radio/oplus_sec.img,611818027635a560cf56f608943533a9560a7c50)
-$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,a3d23f7250c1906c58b73fae4c97aed8f1216c39)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,9b19282f8d6e03876a9e69e20be256b52ebc7db4)
-$(call add-radio-file-sha1-checked,radio/rpm.img,a15c45f107da5b18d0621fb90b86086a4f6ddf1d)
-$(call add-radio-file-sha1-checked,radio/splash.img,cef1d4886175660f305e78ab39b7782f8625e431)
-$(call add-radio-file-sha1-checked,radio/tz.img,33fa80fef11a8e1ac30fd9bf9095ab8e26b34ace)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,45bd1215b77938dd1c4c8c66403ba7d2c3e36a1c)
-$(call add-radio-file-sha1-checked,radio/xbl.img,9bef438ebdc7628e9016a03950fd7ee31f29e64f)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,c5d6dc42bd9ceeef1792c39dc766872f0f3b09a5)
-
-endif
